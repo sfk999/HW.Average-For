6010a28e7bb128a992f12849e232e7a428d27bed
sfk999 <avm-expert@ya.ru>
2022-02-13 22:03:47 +0300

    13.02

---
+++ Average.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        int sum = 0;
+        int sumChet = 0;
+        double j = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число диапазона: ");
+        int i = scanner.nextInt();
+        System.out.println("Введите второе число диапазона ");
+        int k = scanner.nextInt();
+     while (i<=k){
+         sum += i;
+         ++ i;
+         ++ j;
+     }
+
+        System.out.println("Сумма чисел в диапазоне равна " + sum);
+        System.out.println("Среднее арифметическое чисел равно " + sum / j);
+
+
+//        if (i<=k && i % 2 == 0){
+//            sumChet += i;
+//            sum += i;
+//            ++ i;
+//            ++ j;
+//        } else {
+//            sum += i;
+//            ++ i;
+//            ++ j;
+//        }
+//        System.out.println("Среднее арифметическое четных чисел равно " + sumChet / j);
+    }
+
+
+}
