167b4ed6ade8cba5eecd7db85a57707ac077fad1
sfk999 <avm-expert@ya.ru>
2022-02-14 14:58:31 +0700

    14.02

--- HWfor.java
+++ HWfor.java
@@ -1,2 +1,15 @@
 public class HWfor {
+    public static void main(String[] args) {
+
+
+//    public static int getMultiplicity (i) {
+//        int multiplicity;
+//        return (i % 4 == 0);
+//}
+        int i;
+        for (i = 100; i > 0; i = i - 1) ;
+        System.out.println("Цифры, кратные 4 от 100 до 1: " + i);
+
+    }
+
 }
