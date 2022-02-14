807a2dc3f4908c305eb94d1056d418d9a5852d8b
sfk999 <avm-expert@ya.ru>
2022-02-14 13:00:46 +0700

    14.02

--- Average.java
+++ Average.java
@@ -4,34 +4,34 @@ public class Average {
     public static void main(String[] args) {
         int sum = 0;
         int sumChet = 0;
+        double jChet = 0;
         double j = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число диапазона: ");
         int i = scanner.nextInt();
         System.out.println("Введите второе число диапазона ");
         int k = scanner.nextInt();
-     while (i<=k){
-         sum += i;
-         ++ i;
-         ++ j;
-     }
+//     while (i<=k){
+//         sum += i;
+//         ++ i;
+//         ++ j;
+//     }
 
+        while (i <= k) {
+            if (i % 2 == 0) {
+                sumChet += i;
+                sum += i;
+                ++i;
+                ++jChet;
+                ++j;
+            } else {
+                sum += i;
+                ++i;
+                ++j;
+            }
+        }
         System.out.println("Сумма чисел в диапазоне равна " + sum);
         System.out.println("Среднее арифметическое чисел равно " + sum / j);
-
-
-//        if (i<=k && i % 2 == 0){
-//            sumChet += i;
-//            sum += i;
-//            ++ i;
-//            ++ j;
-//        } else {
-//            sum += i;
-//            ++ i;
-//            ++ j;
-//        }
-//        System.out.println("Среднее арифметическое четных чисел равно " + sumChet / j);
+        System.out.println("Среднее арифметическое четных чисел равно " + sumChet / jChet);
     }
-
-
 }
