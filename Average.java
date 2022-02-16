ffceec7d7a6d2d6b96ab41bf3e182fa8e132ea51
sfk999 <avm-expert@ya.ru>
2022-02-16 09:19:33 +0700

    16.02

--- Average.java
+++ Average.java
@@ -3,35 +3,25 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         int sum = 0;
-        int sumChet = 0;
-        double jChet = 0;
-        double j = 0;
+        int sumEvenNumbers = 0;
+        int countEvenNumbers = 0;
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число диапазона: ");
         int i = scanner.nextInt();
         System.out.println("Введите второе число диапазона ");
         int k = scanner.nextInt();
-//     while (i<=k){
-//         sum += i;
-//         ++ i;
-//         ++ j;
-//     }
 
         while (i <= k) {
+            sum += i;
+            ++i;
+            ++count;
             if (i % 2 == 0) {
-                sumChet += i;
-                sum += i;
-                ++i;
-                ++jChet;
-                ++j;
-            } else {
-                sum += i;
-                ++i;
-                ++j;
+                sumEvenNumbers += i;
+                ++countEvenNumbers;
             }
+            }
+        System.out.println("Среднее арифметическое чисел равно " + sum / count);
+        System.out.println("Среднее арифметическое четных чисел равно " + sumEvenNumbers / countEvenNumbers);
         }
-        System.out.println("Сумма чисел в диапазоне равна " + sum);
-        System.out.println("Среднее арифметическое чисел равно " + sum / j);
-        System.out.println("Среднее арифметическое четных чисел равно " + sumChet / jChet);
     }
-}
--- HWfor.java
+++ HWfor.java
@@ -1,18 +1,18 @@
-public class HWfor {
-    public static void main(String[] args) {
-
-
-        for (int j = 100; j >= 1; j--){
-            int multiply = getMultiply;
-            System.out.print(multiply + " ");
-        }
-
-        public static int getMultiply(int j)  {
-            return j if (j % 4 == 0);
-        }
-    }
-
-}
+//public class HWfor {
+//    public static void main(String[] args) {
+//
+//
+//        for (int j = 100; j >= 1; j--){
+//            int multiply = getMultiply;
+//            System.out.print(multiply + " ");
+//        }
+//
+//        public static int getMultiply(int j)  {
+//            return j if (j % 4 == 0);
+//        }
+//    }
+//
+//}
 
 
 //            if (j % 4 == 0)
