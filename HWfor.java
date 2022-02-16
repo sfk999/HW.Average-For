1dfe698832de68df920ec7287f338562797e78eb
sfk999 <avm-expert@ya.ru>
2022-02-16 09:40:38 +0700

    16.02 for

--- HWfor.java
+++ HWfor.java
@@ -1,18 +1,22 @@
-//public class HWfor {
-//    public static void main(String[] args) {
-//
-//
-//        for (int j = 100; j >= 1; j--){
-//            int multiply = getMultiply;
-//            System.out.print(multiply + " ");
-//        }
-//
-//        public static int getMultiply(int j)  {
-//            return j if (j % 4 == 0);
-//        }
-//    }
-//
-//}
+public class HWfor {
+    public static void main(String[] args) {
 
 
-//            if (j % 4 == 0)
+        for (int j = 100; j >= 1; j--){
+            if boolean isMultiplyFour = true;
+            int value;
+            System.out.print(value + " ");
+        }
+
+        public boolean isMultiplyFour {
+            int value;
+            if ((value % 4)== 0){
+                return true;
+            }
+            else { return false; }
+
+    }
+
+}
+
+
